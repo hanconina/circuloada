1fd1c24d28443678be6e2138a321265da17b3baa
user <user@teminal>
2021-02-20 16:15:32 -0500

    Signal method

--- src/listasimple/AppLista.java
+++ src/listasimple/AppLista.java
@@ -3,13 +3,20 @@ package listasimple;
 
 public class AppLista {
     public static void main(String[] args) {
-        Lista<String> lista = new Lista();
+        Lista<String> lista = new Lista<String>();
         lista.agregar("20.4");
         lista.agregar("23.5");
         lista.agregar("2.5");
         lista.agregar("23.7");
         lista.agregar("4.7");
-        
+        lista.mostrar();
+        System.out.println("Longirud: "+lista.longitud());
+        if (lista.iesimo(5)!=null){
+            System.out.println("Iesimo: "+lista.iesimo(5).getDato());
+        }
+        System.out.println("Ubicación: "+lista.ubicacion("20."));
+        System.out.println("Insertar");
+        lista.insertar(6, "a");
         lista.mostrar();
     }
     
--- src/listasimple/Lista.java
+++ src/listasimple/Lista.java
@@ -1,68 +1,110 @@
-
 package listasimple;
-public class Lista <T extends Comparable>{
+
+public class Lista<T> {
+
     // Atributo
-    private Nodo cabeza;  
+    private Nodo<T> cabeza;
+
     // Operaciones
-    public Lista(){
+    public Lista() {
         this.cabeza = null;
     }
+
     // Verificar si la Lista esta vácia
-    public boolean esVacia(){
+    public boolean esVacia() {
         // return cabeza==null;
-        if (cabeza==null){
+        if (cabeza == null) {
             return true;
-        }else{
+        } else {
             return false;
-        }            
+        }
     }
+
     // Agregar un elemento a la Lista
-    public void agregar(T elemento){
+    public void agregar(T elemento) {
         // Agregar un elemento en la primera posición
-        if (esVacia()){
-            Nodo nuevoNodo = new Nodo(elemento,null);
+        if (esVacia()) {
+            Nodo nuevoNodo = new Nodo(elemento, null);
             cabeza = nuevoNodo;
-        }else{ // Tenemos elementos
+        } else { // Tenemos elementos
             Nodo aux = cabeza;
-            while (aux.getSgtNodo() != null){
-               aux = aux.getSgtNodo();
+            while (aux.getSgtNodo() != null) {
+                aux = aux.getSgtNodo();
             }
-            Nodo nuevoNodo = new Nodo(elemento,null);
+            Nodo nuevoNodo = new Nodo(elemento, null);
             aux.setSgtNodo(nuevoNodo);
-        }        
+        }
     }
-    public void mostrar(){
+
+    public void mostrar() {
         Nodo aux = cabeza;
-        while (aux!=null){
+        while (aux != null) {
             System.out.println(aux.getDato());
             aux = aux.getSgtNodo(); // Desplace el puntero            
         }
     }
-    
-    public int lonitud(){
+
+    public int longitud() {
         int n = 0;
         Nodo aux = cabeza;
-        while (aux!=null){
+        while (aux != null) {
             n++;
             aux = aux.getSgtNodo(); // Desplace el puntero            
         }
         return n;
     }
-    
-    public T iesimo(int pos){
+
+    public Nodo iesimo(int pos) {
+        if (pos >= 1 && pos <= longitud()) {
+            int i = 1;
+            Nodo aux = cabeza;
+            while (aux != null) {
+                if (i == pos) {
+                    return aux;
+                }
+                i++;
+                aux = aux.getSgtNodo(); // Desplace el puntero            
+            }
+        }
         return null;
     }
-    
-    public int ubicacion(T elemento){
-        return 0;
+
+    public int ubicacion(T elemento) {
+        int i = 1;
+        Nodo aux = cabeza;
+        while (aux != null) {
+            if (aux.getDato().equals(elemento)) {
+                return i;
+            }
+            aux = aux.getSgtNodo(); // Desplace el puntero
+            i++;
+        }
+        return -1;
     }
-    
-    public void insertar(int pos, T elemento){
-        
+
+    public void insertar(int pos, T elemento) {
+        int n = longitud();
+        if (pos >= 1 && pos <= n) {
+            if (pos == 1) {
+                Nodo aux = cabeza;
+                cabeza = new Nodo(elemento, aux);
+            } else {
+                int i = 1;
+                Nodo aux = cabeza;
+                Nodo nodoAnterior = cabeza;
+                while (i < pos) {
+                    nodoAnterior = aux;
+                    aux = aux.getSgtNodo();
+                    i++;
+                }
+                Nodo nuevoNodo = new Nodo(elemento, aux);
+                nodoAnterior.setSgtNodo(nuevoNodo);                
+            }
+        }
     }
-    
-    public void eliminar(int pos){
-        
+
+    public void eliminar(int pos) {
+
     }
-    
+
 }
